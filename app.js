e5c959d73e7691d4785a7645bf42e9deb1c153eb
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-07-07 16:59:34 -0300

    Corrigida string de conexão do MongoDB Atlas

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import studentRouter from './routes/studentRoutes.js'
-require('dotenv').config();
+// require('dotenv').config();
 
-
+// 'mongodb+srv://'
+//     +process.env.USER+
+//     ':'
+//     +process.env.PASSWORD+
+//     '@'
+//     +process.env.CLUSTER+
+//     '-oclyh.gcp.mongodb.net/'+process.env.DB+'?retryWrites=true&w=majority', 
 
 const app = express();
 app.use(express.json());
 (async () => {
   try {
-    await mongoose.connect(`
-    mongodb+srv://
-    ${process.env.USER}:${process.env.PASSWORD}@
-    ${process.env.CLUSTER}-oclyh.gcp.mongodb.net/
-    ${process.env.DB}?
-    retryWrites=true&w=majority
-    `, {
+    await mongoose.connect('mongodb://'+
+    process.env.MONGODB_ATLAS_USER+
+    ':' + 
+    process.env.PASSWORD+
+    '@fullstackcluster-shard-00-00-oclyh.gcp.mongodb.net:27017,'+
+    'fullstackcluster-shard-00-01-oclyh.gcp.mongodb.net:27017,'+
+    'fullstackcluster-shard-00-02-oclyh.gcp.mongodb.net:27017/'+
+    process.env.DB+
+    '?ssl=true&replicaSet=FullStackCluster-shard-0&authSource=admin&retryWrites=true&w=majority',
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true
     }).then(() =>
@@ -32,4 +41,4 @@ app.use(express.json());
 
 app.use(studentRouter);
 
-app.listen(3333, () => console.log('🌟 API has started, port: 3333'));
+app.listen(process.env.PORT, () => console.log('🌟 API has started, port: 3333'));
